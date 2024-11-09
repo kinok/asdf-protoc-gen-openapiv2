4a28616db22f0361d20c8d476507e598c386d359
michael inthilith <minthilith@gmail.com>
2024-11-09 22:46:58 +0100

    build openapiv2

--- contributing.md
+++ contributing.md
@@ -5,7 +5,6 @@ Testing Locally:
 ```shell
 asdf plugin test <plugin-name> <plugin-url> [--asdf-tool-version <version>] [--asdf-plugin-gitref <git-ref>] [test-command*]
 
-# TODO: adapt this
 asdf plugin test protoc-gen-openapiv2 https://github.com/kinok/asdf-protoc-gen-openapiv2.git "protoc-gen-openapiv2 --help"
 ```
 
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for protoc-gen-openapiv2.
-GH_REPO="https://github.com/kinok/protoc-gen-openapiv2"
+GH_REPO="https://github.com/grpc-ecosystem/grpc-gateway"
 TOOL_NAME="protoc-gen-openapiv2"
 TOOL_TEST="protoc-gen-openapiv2 --help"
 
@@ -26,23 +25,47 @@ sort_versions() {
 
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+		grep -o 'refs/tags/cmd/protoc-gen-openapiv2/.*' | cut -d/ -f5- |
 		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if protoc-gen-openapiv2 has other means of determining installable versions.
 	list_github_tags
 }
 
-download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
+get_platform() {
+  local os=$(uname)
+  if [[ "${os}" == "Darwin" ]]; then
+    echo "darwin"
+  elif [[ "${os}" == "Linux" ]]; then
+    echo "linux"
+  else
+    echo >&2 "unsupported os: ${os}" && exit 1
+  fi
+}
 
-	# TODO: Adapt the release URL convention for protoc-gen-openapiv2
-	url="$GH_REPO/archive/v${version}.tar.gz"
+get_arch() {
+  local os=$(uname)
+  local arch=$(uname -m)
+  # On ARM Macs, uname -m returns "arm64", but in protoc releases this architecture is called "aarch_64"
+  if [[ "${os}" == "Darwin" && "${arch}" == "arm64" ]]; then
+    # echo "aarch_64"
+    echo "x86_64"
+  elif [[ "${os}" == "Linux" && "${arch}" == "aarch64" ]]; then
+    # echo "aarch_64"
+    echo "x86_64"
+  else
+    echo "${arch}"
+  fi
+}
+
+download_release() {
+  local version filename url
+  version="$1"
+  filename="$2"
+
+  url="$GH_REPO/releases/download/${version}/$TOOL_NAME-${version}-$(get_platform)-$(get_arch)"
+
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -59,9 +82,8 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		cp -r "$ASDF_DOWNLOAD_PATH"/cmd/$TOOL_NAME/$TOOL_NAME "$install_path"
 
-		# TODO: Assert protoc-gen-openapiv2 executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
